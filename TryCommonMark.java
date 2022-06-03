97f3760969036d2fd53ddc1e77c7be7699705d51
l-j-kim <103155974+l-j-kim@users.noreply.github.com>
2022-06-03 08:38:22 -0700

    fix trycommonmark

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -2,7 +2,18 @@ import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
+class TryCommonMark {
+    public static void main(String[] args) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse("This is *Sparta*");
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+    }
+}
+
+/* 
 Parser parser = Parser.builder().build();
 Node document = parser.parse("This is *Sparta*");
 HtmlRenderer renderer = HtmlRenderer.builder().build();
 renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+*/
